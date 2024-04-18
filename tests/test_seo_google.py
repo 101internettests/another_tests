920c85cd6ff53109b856c699e670a87aabab23ac
EkaterinaMangileva <mangileva.ek@gmail.com>
2024-04-19 01:08:57 +0500

    new

--- tests/test_seo_google.py
+++ tests/test_seo_google.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.by import By
 import time
 import pytest
 from selenium import webdriver
+from pages.providers_tests.data import query_new
 
 @pytest.fixture
 def browser():
@@ -14,32 +15,25 @@ def browser():
     driver.quit()
 
 
-def test_find_competitors_and_website_rank(browser):
-    search_queries = ["101интернет", "мтс подключить", "питер онлайн"]
-    website_to_check = "101internet.ru"  # ваш веб-сайт, позицию которого вы хотите найти
+def test_find_competitors_and_website_rank(driver):
+    websites_to_check = ["101internet", "piter-online","moskvaonline"]
+    for website_to_check in websites_to_check:
+        for query in query_new:
+            driver.get(f"https://www.google.com/search?q={query}")
+            search_results = driver.find_elements(By.CSS_SELECTOR, "div.g")
+            website_rank = None
 
-    for query in search_queries:
-        browser.get(f"https://www.google.com/search?q={query}")
-
-        search_results = browser.find_elements(By.CSS_SELECTOR, "div.g")
-
-        competitors = []
-        website_rank = None
-        for index, result in enumerate(search_results, start=1):
-            link = result.find_elements(By.CSS_SELECTOR, "a")
-            if link:
-                url = link[0].get_attribute("href")
-                if url:  # Добавим дополнительную проверку на наличие URL
-                    title = link[0].text
-                    if website_to_check in url:
+            for index, result in enumerate(search_results, start=1):
+                link = result.find_elements(By.CSS_SELECTOR, "a")
+                if link:
+                    url = link[0].get_attribute("href")
+                    if url is not None and any(website in url for website in websites_to_check):
                         website_rank = index
-                    competitors.append(title)
+                        break
 
-        assert competitors, f"No search results found for query '{query}'"
+            if website_rank:
+                print(f"Сайт '{website_to_check}' был на позиции {website_rank} в результатах по запросу '{query}'.")
+            else:
+                print(f"Сайт '{website_to_check}' не был найден в результатах по запросу '{query}'.")
 
-        if website_rank:
-            print(f"Ваш сайт '{website_to_check}' был на позиции {website_rank} в результатах по запросу '{query}'.")
-        else:
-            print(f"Сайт '{website_to_check}' не был найден в результатах по запросу '{query}'.")
-
-        time.sleep(2)  # Добавим паузу между запросами, чтобы не нагружать сервер Google слишком частыми запросами
+            time.sleep(2)
