9f9ebe569d6d3ed4c362cef3859244f08497ffc9
EkaterinaMangileva <mangileva.ek@gmail.com>
2024-05-15 12:45:11 +0500

    new

--- tests/test_seo_google.py
+++ tests/test_seo_google.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 import time
 import pytest
 from selenium import webdriver
-from pages.providers_tests.data import query_new, query_new_spb, query_new_msk
+from pages.providers_tests.data import query_new, query_new_spb, query_new_msk, query_new_ekb
 import csv
 import time
 from selenium.webdriver.common.by import By
@@ -38,6 +38,7 @@ def test_find_competitors_and_website_rank(driver):
 
     df = pd.DataFrame(results, columns=['Сайт', 'Запрос', 'Место в поиске', 'Ссылка'])
     df.to_excel('website_results.xlsx', index=False)
+
 
 def test_find_competitors_and_website_rank_spb(driver):
     websites_to_check = ["101internet", "moskvaonline", "piter-online"]
@@ -95,3 +96,32 @@ def test_find_competitors_and_website_rank_msk(driver):
 
     df = pd.DataFrame(results, columns=['Сайт', 'Запрос', 'Место в поиске', 'Ссылка'])
     df.to_excel('website_results_msk.xlsx', index=False)
+
+
+def test_find_competitors_and_website_rank_ekb(driver):
+    websites_to_check = ["101internet", "moskvaonline", "piter-online"]
+    results = []
+    for website_to_check in websites_to_check:
+        for query in query_new_ekb:
+            driver.get(f"https://www.google.com/search?q={query}")
+            search_results = driver.find_elements(By.CSS_SELECTOR, "div.g")
+            website_rank = None
+            link = None
+
+            for index, result in enumerate(search_results, start=1):
+                link_elements = result.find_elements(By.CSS_SELECTOR, "a")
+                if link_elements:
+                    url = link_elements[0].get_attribute("href")
+                    if url and any(website in url for website in websites_to_check):
+                        website_rank = index
+                        link = url
+                        break
+
+            if website_rank is not None and link is not None:
+                results.append([website_to_check, query, website_rank, link])
+            else:
+                results.append([website_to_check, query, 'не найдено', 'нет ссылки'])
+            time.sleep(2)
+
+    df = pd.DataFrame(results, columns=['Сайт', 'Запрос', 'Место в поиске', 'Ссылка'])
+    df.to_excel('website_results_ekb.xlsx', index=False)
