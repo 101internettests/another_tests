2e779efaa064a2214e1e46c933e0973d051a46e0
EkaterinaMangileva <mangileva.ek@gmail.com>
2024-05-28 13:51:52 +0500

    Обновила парсеров про просьбам СЕО

--- tests/test_seo_yandex.py
+++ tests/test_seo_yandex.py
@@ -6,40 +6,38 @@ from pages.providers_tests.data import urlsyandex, path_yandex, url_ms, path_yan
 import pandas as pd
 
 
-websites_to_check = ["101internet.ru", "piter-online.net", "moskvaonline.ru"]
-
-
-def test_find_competitors_and_website_rank(driver):
-    results = []
-    for website_to_check in websites_to_check:
-        for url, filename in zip(urlsyandex, path_yandex):
-            driver.get(url)
-            search_results = driver.find_elements(By.CSS_SELECTOR, "li[data-cid]")
-            website_rank = None
-            link = None
-
-            for index, result in enumerate(search_results, start=1):
-                link_elements = result.find_elements(By.CSS_SELECTOR, "a")
-                if link_elements:
-                    url = link_elements[0].get_attribute("href")
-                    if url and any(website in url for website in websites_to_check):
-                        website_rank = index
-                        link = url
-                        break
-
-            if website_rank is not None and link is not None:
-                results.append([website_to_check, filename, website_rank, link])
-            else:
-                results.append([website_to_check, filename, 'не найдено', 'нет ссылки'])
-
-            time.sleep(2)
-
-    df = pd.DataFrame(results, columns=['Сайт', 'Запрос', 'Место в поиске', 'Ссылка'])
-    df.to_excel('yandex.xlsx', index=False)
+# def test_find_competitors_and_website_rank(driver):
+#     results = []
+#     for website_to_check in websites_to_check:
+#         for url, filename in zip(urlsyandex, path_yandex):
+#             driver.get(url)
+#             search_results = driver.find_elements(By.CSS_SELECTOR, "li[data-cid]")
+#             website_rank = None
+#             link = None
+#
+#             for index, result in enumerate(search_results, start=1):
+#                 link_elements = result.find_elements(By.CSS_SELECTOR, "a")
+#                 if link_elements:
+#                     url = link_elements[0].get_attribute("href")
+#                     if url and any(website in url for website in websites_to_check):
+#                         website_rank = index
+#                         link = url
+#                         break
+#
+#             if website_rank is not None and link is not None:
+#                 results.append([website_to_check, filename, website_rank, link])
+#             else:
+#                 results.append([website_to_check, filename, 'не найдено', 'нет ссылки'])
+#
+#             time.sleep(2)
+#
+#     df = pd.DataFrame(results, columns=['Сайт', 'Запрос', 'Место в поиске', 'Ссылка'])
+#     df.to_excel('yandex.xlsx', index=False)
 
 
 def test_find_competitors_and_website_rank_msk(driver):
     results = []
+    websites_to_check = ["moskvaonline.ru"]
     for website_to_check in websites_to_check:
         for url, filename in zip(url_ms, path_yandex_msk):
             driver.get(url)
@@ -69,6 +67,7 @@ def test_find_competitors_and_website_rank_msk(driver):
 
 def test_find_competitors_and_website_rank_spb(driver):
     results = []
+    websites_to_check = ["piter-online.net"]
     for website_to_check in websites_to_check:
         for url, filename in zip(url_spb, path_yandex_spb):
             driver.get(url)
@@ -98,6 +97,7 @@ def test_find_competitors_and_website_rank_spb(driver):
 
 def test_find_competitors_and_website_rank_ufa(driver):
     results = []
+    websites_to_check = ["101internet.ru"]
     for website_to_check in websites_to_check:
         for url, filename in zip(url_ufa, path_yandex_krasnodar):
             driver.get(url)
@@ -127,6 +127,7 @@ def test_find_competitors_and_website_rank_ufa(driver):
 
 def test_find_competitors_and_website_rank_krasnodar(driver):
     results = []
+    websites_to_check = ["101internet.ru"]
     for website_to_check in websites_to_check:
         for url, filename in zip(url_krasnodar, path_yandex_krasnodar):
             driver.get(url)
@@ -156,6 +157,7 @@ def test_find_competitors_and_website_rank_krasnodar(driver):
 
 def test_find_competitors_and_website_rank_novosibirsk(driver):
     results = []
+    websites_to_check = ["101internet.ru"]
     for website_to_check in websites_to_check:
         for url, filename in zip(url_novosibirsk, path_yandex_novosibirsk):
             driver.get(url)
