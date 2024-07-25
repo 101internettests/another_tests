51f61ed33eeb68472c051b6101cb098da1af9794
EkaterinaMangileva <mangileva.ek@gmail.com>
2024-07-25 21:57:01 +0500

    Обновления парсеров

--- pages/seo/parser_page.py
+++ pages/seo/parser_page.py
@@ -6,11 +6,23 @@ results = []
 
 
 class SeoPage(BasePage):
+    results = []  # Переменная класса для хранения результатов
+
     def seo_func(self):
-        h1 = self.element_is_visible(SeoLocators.H1).text
-        title = self.element_is_present(SeoLocators.TITLE).text
-        print(h1)
-        print(title)
-        results.append([h1, title])
-        df = pd.DataFrame(results, columns=['h1', 'title'])
-        df.to_excel('test.xlsx', index=False)
+        url_before_redirect = self.driver.current_url
+
+        try:
+            if self.element_is_visible(SeoLocators.H1):
+                h1 = self.element_is_visible(SeoLocators.H1).text
+            else:
+                h1 = "заголовка нет"  # Если H1 не найден, записываем текст
+
+            self.results.append([h1, url_before_redirect])
+
+            # Сохраняем результаты в DataFrame и экспортируем в Excel
+            df = pd.DataFrame(self.results, columns=['h1', 'url_before_redirect'])
+            df.to_excel('test.xlsx', index=False)
+
+        except Exception as e:
+            print(f"Ошибка: {e}. Пошел дальше.")
+
